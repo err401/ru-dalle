b8d9d8f843e81e711a0eee1409d6f14b455fc9ad
err401 <regulus-yokel6x@icloud.com>
2025-01-22 17:23:34 +0100

    Update __init__.py

--- rudalle/dalle/__init__.py
+++ rudalle/dalle/__init__.py
@@ -188,7 +188,7 @@ def get_rudalle_model(name, pretrained=True, fp16=False, device='cpu', use_auth_
         cache_dir = os.path.join(cache_dir, name)
         config_file_url = hf_hub_url(repo_id=config['repo_id'], filename=config['filename'])
         try:
-            hf_hub_download(repo_id=config['repo_id'], filename=config['filename'], cache_dir=cache_dir, force_filename=config['filename'],
+            hf_hub_download(repo_id=config['repo_id'], filename=config['filename'], local_dir=cache_dir, force_filename=config['filename'],
                             use_auth_token=use_auth_token)
         except requests.HTTPError as err:
             err_str = str(err)
